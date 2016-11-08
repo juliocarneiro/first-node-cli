6c2b19ca18bc0f9af9bd9401aa8c7861421b23f7
juliocarneiro <juliocarneiro@gmail.com>
2016-11-07 23:57:27 -0200

    github oauth

--- index.js
+++ index.js
@@ -1,14 +1,7 @@
-const axios = require('axios')
-const config = require('./config')
-const url = 'https://api.github.com/users/juliocarneiro'
-const headers = {
-	Authorization: 'token ${config.GITHUB_OAUTH_TOKEN}'
-}
+const http = require('./clientHttp')
 
-axios.get(url, { headers })
+http.get('/users/juliocarneiro')
 .then(response => response.data)
 .then(data => {
-	console.log(data.name)
-	console.log(data.location)
-	console.log(data.bio)
+	console.log(data)
 })
